92b756b16280a6bad820c7c03b05556c0daa32f2
ilyasse0 <ilyassebensitel@gmail.com>
2024-09-01 00:00:24 +0200

    update the app file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
-import React, { Component } from 'react';
+import React from 'react'; // Removed unnecessary import { Component }
 import './App.css';
 import axios from 'axios';
 
 class App extends React.Component {
 
   state = { advice: '' };
+
   componentDidMount() {
     this.fetchAdvice();
   }
@@ -14,26 +14,24 @@ class App extends React.Component {
     axios.get('https://api.adviceslip.com/advice')
       .then((response) => {
         const { advice } = response.data.slip;
-        this.setState({advice });
+        this.setState({ advice });
       })
       .catch((error) => {
         console.error('Error fetching data', error);
       });
-
   }
+
   render() {
-    const {advice} = this.state;
+    const { advice } = this.state;
     return (
       <div className='app'>
         <div className='card'>
-            <h1 className='heading'>{advice}</h1>
-            <button className='button'  onClick={this.fetchAdvice}> 
-              <span>New Advice</span>
-            </button>
+          <h1 className='heading'>{advice}</h1>
+          <button className='button' onClick={this.fetchAdvice}>
+            <span>New Advice</span>
+          </button>
         </div>
       </div>
-
-
     );
   }
 }
